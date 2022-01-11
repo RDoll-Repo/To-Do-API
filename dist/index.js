fd7110f60df26c42ed67568c7cb359586273251c
Roland Doll <rdoll007@gmail.com>
2022-01-11 16:10:34 -0500

    Start TypeScript Support

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var testString = "If you see this, I have been able to successfully integrate typescript into my project.";
+console.log(testString);
---
+++ index.ts
@@ -0,0 +1,3 @@
+var testString = "i h8 getsuyo";
+
+console.log(testString);
