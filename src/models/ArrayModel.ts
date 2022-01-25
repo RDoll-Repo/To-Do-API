fdfec62efdb7c309b3098c3e16b9717ee196d61e
Roland Doll <rdoll007@gmail.com>
2022-01-25 14:49:14 -0500

    organize Prototype API components

---
+++ src/models/ArrayModel.ts
@@ -0,0 +1,8 @@
+// Array to prototype hapi + postman with. 
+export let Tasks =
+[
+    {id: 1, taskDescription:"TypeScript", createdDate:"1/10/2021", dueDate:"1/14/2021", completed:true},
+    {id: 2, taskDescription:"Hapi", createdDate:"1/10/2021", dueDate:"1/14/2021", completed:false},
+    {id: 3, taskDescription: "Prototype API", createdDate:"1/12/2022", dueDate: "1/14/2022",completed:false},
+    {id: 4, taskDescription: "Trash", createdDate:"1/14/2022", dueDate: "1/26/2025",completed:false}
+];
