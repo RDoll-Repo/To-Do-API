8d8f1806c54e2ebd0c03db4695049422af89839a
Roland Doll <rdoll007@gmail.com>
2022-02-25 15:29:03 -0500

    implement sequelize models

---
+++ src/models/SequelizeModels.ts
@@ -0,0 +1,36 @@
+const instances = require('hapi-sequelizejs').instances;
+import { DataTypes, Sequelize } from "sequelize";
+
+module.exports.TaskModel = function () {
+    const Task = instances.dbs.ToDoAPI.sequelize.define('Tasks', { 
+        id: {
+            // ID as auto-generated UUID
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        taskDescription: {
+            // 255 chars should be sufficient.
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dueDate: {
+            // Sequelize gives createdAt by default, but we still need due date
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        completed: {
+            // Default to false (incomplete) since the user likely wouldn't put complete items on the list
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        } 
+    }, 
+    {
+        freezeTableName: true,
+        tableName: 'Task',
+        updatedAt: false
+    })
+    return Task
+}
+
--- src/services/TaskRepository.ts
+++ src/services/TaskRepository.ts
@@ -1,83 +1,69 @@
-import { QueryTypes } from "sequelize";
+import { v4 as uuidv4 } from 'uuid';
+const Task = require('../models/SequelizeModels');
 
-const instances = require('hapi-sequelizejs').instances;
 export class TaskRepo {
-    
+
     async getTasks(completion:string, sortBy:string, sortOrder:string){
-        var queryString = "SELECT * FROM Tasks ";
+        // Add filtering and sorting 
+        const query = Task.TaskModel()
 
-        if (completion != null && completion != undefined)
-        {
-            queryString += `WHERE completed = ${completion} `;
+        const results = query.findAll()
 
-        }
-
-        if (sortBy != null && sortBy != undefined)
-        {
-            queryString += `ORDER BY ${sortBy} ${sortOrder}`;
-        }
-        
-        var [results] = await instances.dbs.ToDoAPI.sequelize.query(
-            queryString
-        )
-        return results;
-    }
-
-    async fetchTask(id:number) {
-        const [results] = await instances.dbs.ToDoAPI.sequelize.query(
-            'SELECT * FROM Tasks WHERE id = ?',
-            {
-                replacements: [id],
-                type: QueryTypes.SELECT
-            }
-        )
-        return results;
-    }
-
-    async createTask(desc:string, due:Date, completed: boolean){
-        const newTask:ITask = {
-            id: 0,
-            taskDescription:desc, 
-            createdAt: new Date(),
-            dueDate:due, 
-            completed:completed
-        }
-        const created = newTask.createdAt.toISOString().split('T')[0]
-
-        const [results] = await instances.dbs.ToDoAPI.sequelize.query(
-            'INSERT INTO Tasks (taskDescription, createdAt, dueDate, completed) '+
-            `VALUES (?, ?, ?, ?)`,
-            {
-                replacements:[desc, created, due, completed],
-                type: QueryTypes.INSERT
-            }
-        )
-        return results;
-    }
-
-    async updateTask(id:number, desc:string, due:Date, completed: boolean) {
-        const [results] = await instances.dbs.ToDoAPI.sequelize.query(
-            'UPDATE Tasks SET ' +
-            `taskDescription = ?, ` +
-            `dueDate = ?, ` +
-            `completed = ? ` +
-            `WHERE id = ?`,
-            {
-                replacements: [desc, due, completed, id],
-                type: QueryTypes.UPDATE
-            }
-        )
         return results
     }
 
-    async deleteTask(id:number) { 
-        const [results] = await instances.dbs.ToDoAPI.sequelize.query(
-            'DELETE FROM Tasks WHERE id = ?',
-            {
-                replacements: [id],
+    async fetchTask(fetchID:number) {
+        const query = Task.TaskModel()
+
+        const results = query.findAll({
+            where: {
+                id: fetchID
             }
-        )
-        return results;
+        })
+
+        return results
+    }
+
+    async createTask(desc:string, due:Date, completed: boolean){
+
+        const query = Task.TaskModel()
+
+        const newTask = await query.create({
+            id: uuidv4(),
+            taskDescription:desc, 
+            dueDate:due, 
+            completed:completed
+        })
+        return newTask
+    }
+
+    async updateTask(fetchID:number, desc:string, due:Date, completed: boolean) {
+        const query = Task.TaskModel()
+
+        const updatedTask = await query.update({
+            taskDescription: desc,
+            dueDate: due,
+            completed: completed
+        },
+        {
+            where: {
+                id: fetchID
+            }
+        })
+
+        return updatedTask
+    }
+
+    async deleteTask(fetchID:number) { 
+        const query = Task.TaskModel()
+
+        const results = query.destroy({
+            where: {
+                id: fetchID
+            }
+        })
+
+        return results
     }
 }
 
@@ -89,39 +75,7 @@ interface ITask {
     completed: boolean;
 }
 
+
+
+
 export const repo = new TaskRepo
-
-
-// TO DO (during models): 
-// -Convert IDs to GUID/UUID once models are being used. 
-// -Autogen createdAt
-
-// No need for a model at the moment, but keeping this commented out until its needed.
-// const Task = Sequelize.define('Task', { 
-//     id: {
-//         // ID as UUID, Sequelize can autogenerate a UUID
-//         type: DataTypes.UUID,
-//         allowNull: false,
-//         defaultValue: DataTypes.UUIDV4,
-//         primaryKey: true
-//     },
-//     taskDescription: {
-//         // 255 chars should be sufficient.
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     dueDate: {
-//         // Sequelize gives createdAt by default, but we still need due date
-//         type: DataTypes.DATEONLY,
-//         allowNull: false
-//     },
-//     completed: {
-//         // Default to false (incomplete) since the user likely wouldn't put incomplete items on the list
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: false
-//     } 
-// }, {
-//     updatedAt:false
-// });
-
